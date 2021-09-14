1f8fe10ad65dcbfd195e52ce2811e5a0ef26d412
Sheeeev66 <roi.hakim@gmail.com>
2021-09-14 19:38:50 +0300

    setting up enviroment

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -14,23 +14,24 @@ contract LazyAst is Ownable, ERC721, IERC2981 {
 
     // Public address of the royalty reciever:
     address private royaltyReciever;
+    // base URI
+    string private baseURIcid;
+    // loanched:
+    bool loanched;
 
     using Counters for Counters.Counter;
     using Strings for uint256;
 
-    event NewLaMinted(uint id);
+    event NewMinted(uint id);
     event Withdrawn(address _address, uint amount);
     
     // track token ID
     Counters.Counter private _tokenId;
 
-    // base URI
-    string private baseURIcid;
+    
 
-    // check if the team minted or not
-    bool teamMintAvalible = true;
 
-    constructor() ERC721("Lazy Ast", "LA") { }
+    constructor() ERC721("Name", "AAA") { }
 
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC721, IERC165) returns (bool) {
@@ -71,20 +72,36 @@ contract LazyAst is Ownable, ERC721, IERC2981 {
 
     /**
      * @dev premint for team
-     * @dev 100 will be minted for the team
+     * @dev 20 will be minted for the team
+     * @notice Can be only be called before before tokens are minted and 
      * @notice the NFTs will be minted and sent to the callers address
      * @notice only the owner of the contract can call this function
      */
-    function teamMint() external onlyOwner {
+    function teamMintAndStartPremint() external onlyOwner {
         require(bytes(baseURIcid).length > 0, "No IPFS CID set. Minting will be enabled once setBaseURIcid(cid) will be called");
-        require(teamMintAvalible, "LazyAst: Team Mint Has already happened");
-        for (uint i=0; i < 100; i++) {
+        require(_tokenId.current() == 0, "Team Mint Has already happened");
+
+        for (uint i=0; i <= 19; i++) {
             _safeMint(msg.sender, _tokenId.current());
 
-            emit NewLaMinted(_tokenId.current());
+            emit NewMinted(_tokenId.current());
             _tokenId.increment();
         }
-        teamMintAvalible = false;
+    }
+
+    function preMint(uint _tokenCount) public payable {
+        require(_tokenId.current() >= 19, "Pre mint has not started yet");
+        require(_tokenCount <= 20, "Can not mint more than 20 tokens at a time");
+        require(_tokenCount != 0, "You have to mint at least 1 token");
+        require(_tokenId.current() + _tokenCount <= 1000, "Purchace will exeed pre mint supply");
+        require(msg.value == 50000000000000000*_tokenCount, "Ether value sent is not correct"); // price for 1: 0.05 eth
+
+        for (uint i=0; i < _tokenCount; i++) {
+            _safeMint(msg.sender, _tokenId.current());
+
+            emit NewMinted(_tokenId.current());
+            _tokenId.increment();
+        }
     }
 
     /**
@@ -95,17 +112,16 @@ contract LazyAst is Ownable, ERC721, IERC2981 {
      * @param _tokenCount the ammount of tokens to mint
      */
     function safeMintLa(uint _tokenCount) public payable {
-        require(bytes(baseURIcid).length > 0, "LazyAst: No IPFS CID set. Minting will be enabled once setBaseURIcid(cid) will be called");
-        require(_tokenCount <= 20, "LazyAst: Can not mint more than 20 tokens at a time");
-        require(_tokenCount != 0, "LazyAst: You have to mint at least 1 token");
-        require(msg.value >= 50000000000000000*_tokenCount, "LazyAst: Ether value sent is not correct"); // price for 1: 0.05 eth
+        require(bytes(baseURIcid).length > 0, "No IPFS CID set. Minting will be enabled once setBaseURIcid(cid) will be called");
+        require(_tokenCount <= 20, "Can not mint more than 20 tokens at a time");
+        require(_tokenCount != 0, "You have to mint at least 1 token");
+        require(_tokenId.current() + _tokenCount <= 8980, "Purchace will exeed max supply of tokens");
+        require(msg.value == 50000000000000000*_tokenCount, "Ether value sent is not correct"); // price for 1: 0.05 eth
 
         for (uint i=0; i < _tokenCount; i++) {
-            require(_tokenId.current() <= 9899, "LazyAst: No more tokens avalible");
-
             _safeMint(msg.sender, _tokenId.current());
 
-            emit NewLaMinted(_tokenId.current());
+            emit NewMinted(_tokenId.current());
             _tokenId.increment();
         }
     }
@@ -119,7 +135,7 @@ contract LazyAst is Ownable, ERC721, IERC2981 {
      */
     function royaltyInfo(uint256 tokenId, uint256 salePrice) external view override returns (address receiver, uint256 royaltyAmount) {
         require(_exists(tokenId), "ERC2981RoyaltyStandard: Royalty info for nonexistent token");
-        return (royaltyReciever, (salePrice * 300) / 10000);
+        return (royaltyReciever, (salePrice * 300) / 10000); // 3 percent
     }
     
     /**
@@ -128,7 +144,7 @@ contract LazyAst is Ownable, ERC721, IERC2981 {
      * @notice Setting the recieving address to the zero address will result in an error
      */
     function setRoyaltyRecieverTo(address _address) public onlyOwner {
-        require(_address != address(0), "LazyAst: Can not send royalties to the zero address");
+        require(_address != address(0), "Can not send royalties to the zero address");
         royaltyReciever = _address;
     }
 
